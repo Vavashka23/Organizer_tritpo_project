b98768d16f8e96ac80edb1f9821106f2ebcf96ed
Vavashka23 <vova.kupreev.23@mail.ru>
2018-11-05 22:56:20 +0300

    add login window

---
+++ Organizer/Organizer/LoginWindow.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Organizer
+{
+    public partial class LoginWindow : Form
+    {
+        private StartWindow startWindow;
+
+        public LoginWindow(StartWindow start)
+        {
+            startWindow = start;
+            InitializeComponent();
+        }
+
+        private void backButton_Click(object sender, EventArgs e)
+        {
+            this.Visible = false;
+            startWindow.Visible = true;
+        }
+    }
+}
---
+++ Organizer/Organizer/StartWindow.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Organizer
+{
+    public partial class StartWindow : Form
+    {
+
+        private LoginWindow loginWindow;
+
+        public StartWindow()
+        {
+            InitializeComponent();
+            loginWindow = new LoginWindow(this) { Visible = false };
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void loginButton_Click(object sender, EventArgs e)
+        {
+            this.Visible = false;
+            loginWindow.Visible = true;
+        }
+
+        private void LoginWindow_FormClose(object sender, FormClosedEventArgs e)
+        {
+            loginWindow.Close();
+            this.Close();
+        }
+    }
+}
