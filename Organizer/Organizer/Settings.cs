66f610af998e6cac17cf7c8e79ccb0db254a0189
Vavashka23 <vova.kupreev.23@mail.ru>
2018-11-22 18:37:16 +0300

    fix component diagram

--- Organizer/Organizer/Settings.cs
+++ Organizer/Organizer/Settings.cs
@@ -1,13 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Data;
+﻿using System.Data;
 using System.Data.Common;
 using System.Data.SqlClient;
 using System.Drawing;
 using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Organizer
 {
